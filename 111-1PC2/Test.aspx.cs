086bc969944d4ad1d31bc896bb78f3236e6a9c77
s9606105 <s9606105@gmail.com>
2022-09-24 20:25:31 +0800

    1110924 Write pdf and markdown

---
+++ 111-1PC2/Test.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1PC2
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int i_In = 66377;
+            double d_Ou = (double)i_In * 9 / 5 + 32;
+            Response.Write("攝氏 "+i_In + " 度<br>" + 
+                "華氏 "+ d_Ou.ToString("0.##") + " 度");
+        }
+    }
+}
